404cd03c69ca8c7deaaebb293a67beeb84a4e300
HenryLee <henrylee_cn@foxmail.com>
2016-04-01 15:55:50 +0800

    路由测试成功

--- System/SysRouter_test.go
+++ System/SysRouter_test.go
@@ -7,7 +7,12 @@ import (
 
 func TestRouter(t *testing.T) {
 	for _, r := range lessgo.DynaRouterTree() {
-		t.Logf("%v\n", r.Url())
+		// t.Logf("%v\n", r.Url)
 		t.Logf("%#v\n\n", r)
 	}
+	lessgo.ResetRealRoute()
+	t.Logf("%#v\n\n", lessgo.DefLessgo.Echo.Routes())
+	lessgo.DelRouter("c920a17fdc112cd0fd9a741ff6e3c7ae")
+	lessgo.ResetRealRoute()
+	t.Logf("%#v\n\n", lessgo.DefLessgo.Echo.Routes())
 }
