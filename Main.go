3993b2f6866895a435cff87c2dd62a48cef1905b
HenryLee <henrylee_cn@foxmail.com>
2016-05-18 01:16:08 +0800

    update

--- Main.go
+++ Main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-    "github.com/lessgo/lessgo"
-    "github.com/lessgo/lessgoext/swagger"
+	"github.com/lessgo/lessgo"
+	"github.com/lessgo/lessgoext/swagger"
 
-    _ "github.com/lessgo/demo/BusinessAPI"
-    _ "github.com/lessgo/demo/Common/Middleware"
-    _ "github.com/lessgo/demo/SystemAPI"
+	_ "github.com/lessgo/demo/BusinessAPI"
+	_ "github.com/lessgo/demo/Common/Middleware"
+	_ "github.com/lessgo/demo/SystemAPI"
 )
 
 func main() {
-    swagger.Init()
-    lessgo.SetHome("/home")
-    lessgo.Run()
+	// 开启自动api文档，false表示仅允许内网访问
+	swagger.Reg(false)
+	// 指定根目录URL
+	lessgo.SetHome("/home")
+	// 开启网络服务
+	lessgo.Run()
 }
