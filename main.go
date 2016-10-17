26bba2e86de480c7dcd0e8fbd72396e43a8068ce
HenryLee <henrylee_cn@foxmail.com>
2016-10-17 09:27:59 +0800

    update swagger

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 
 	// _ "github.com/lessgo/lessgoext/dbservice/gorm"
 	// _ "github.com/lessgo/lessgoext/dbservice/xorm"
+	_ "github.com/lessgo/lessgoext/dbservice/sqlx"
 
 	_ "github.com/lessgo/demo/middleware"
 	_ "github.com/lessgo/demo/router"
 )
 
 func main() {
-	// 开启自动api文档
-	// 参数为true表示自定义允许访问的ip前缀
-	// 参数为false表示只允许局域网访问
-	swagger.Reg(true)
+	// 开启自动api文档，通过config/apidoc_allow.myconfig进行配置
+	swagger.Reg()
 	// 指定根目录URL
 	lessgo.SetHome("/home")
 	// 开启网络服务
